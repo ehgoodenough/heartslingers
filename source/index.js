dff97bc16f8219ee7607b95171b39e6863958635
Andrew McPherson <ehgoodenough@gmail.com>
2017-07-11 22:26:04 -0700

    Added some music in a jukebox.

--- source/index.js
+++ source/index.js
@@ -9,3 +9,12 @@ var loop = new Yaafloop(function(delta) {
     game.update(delta)
     game.render()
 })
+
+import {STAGE} from "scripts/Constants.js"
+import Jukebox from "scripts/Jukebox.js"
+
+if(STAGE === "PRODUCTION") {
+    var jukebox = new Jukebox([
+        new Audio(require("music/procrastinator-elevator.mp3")),
+    ])
+}
---
+++ source/scripts/Jukebox.js
@@ -0,0 +1,36 @@
+const DEFAULT_VOLUME = 0.1
+
+export default class Jukebox {
+    constructor(music) {
+        if(music instanceof Array == false) {
+            music = new Array(music)
+        }
+
+        this.music = music
+
+        this.music.forEach((music) => {
+            music.volume = DEFAULT_VOLUME
+            music.addEventListener("ended", (event) => {
+                this.play()
+            })
+        })
+
+        this.play()
+    }
+    play() {
+        if(this.music.length > 0) {
+            var music = this.music.shift()
+            console.log("Playing", music.src)
+            music.play()
+            this.music.push(music)
+        }
+    }
+}
+
+// var currentTime = Date.now()
+// var startTime = window.localStorage.getItem("startTime")
+// if(startTime != undefined) {
+//     music.currentTime = ((currentTime - startTime) / 1000) % music.duration
+// } else {
+//     window.localStorage.setItem("startTime", currentTime)
+// }
