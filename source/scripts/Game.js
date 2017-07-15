315361630fec90a93d885bf699bd15f0e6dcefcc
Andrew McPherson <ehgoodenough@gmail.com>
2017-07-14 19:52:46 -0700

    Resized the player, and gave them velocity.

--- source/scripts/Game.js
+++ source/scripts/Game.js
@@ -16,10 +16,6 @@ export default class Game extends Pixi.Container {
         Pixi.settings.SCALE_MODE = Pixi.SCALE_MODES.NEAREST
 
         this.addChild(this.player = new Player())
-        this.addChild(this.text = new Text("NO WAY! WHAT!"))
-
-        this.text.position.x = WIDTH / 2
-        this.text.position.y = HEIGHT / 2
     }
     update(delta) {
         this.children.forEach((child) => {
--- source/scripts/Player.js
+++ source/scripts/Player.js
@@ -1,37 +1,48 @@
 import * as Pixi from "pixi.js"
 import Keyb from "keyb"
 
+const FRICTION = 0.75
+
 export default class Player extends Pixi.Sprite {
     constructor() {
         var texture = Pixi.Texture.from(require("images/pixel.png"))
         super(texture)
 
-        this.width = 32
-        this.height = 32
+        this.width = 16
+        this.height = 16
 
-        this.position.y = 32
-        this.position.x = 32
+        this.position.y = 16
+        this.position.x = 16
 
         this.anchor.x = 0.5
         this.anchor.y = 0.5
 
         this.speed = 5
+
+        this.velocity = new Pixi.Point()
     }
     update(delta) {
+        // Acceleration from input.
+        // TODO: https://github.com/ehgoodenough/gmtk-2017/issues/1
         if(Keyb.isDown("<up>") || Keyb.isDown("W")) {
-            this.position.y -= this.speed * delta.f
+            this.velocity.y = -1 * this.speed
         }
-
         if(Keyb.isDown("<down>") || Keyb.isDown("S")) {
-            this.position.y += this.speed * delta.f
+            this.velocity.y = +1 * this.speed
         }
-
         if(Keyb.isDown("<left>") || Keyb.isDown("A")) {
-            this.position.x -= this.speed * delta.f
+            this.velocity.x = -1 * this.speed
+        }
+        if(Keyb.isDown("<right>") || Keyb.isDown("D")) {
+            this.velocity.x = +1 * this.speed
         }
 
-        if(Keyb.isDown("<right>") || Keyb.isDown("D")) {
-            this.position.x += this.speed * delta.f
-        }
+        // Translation of position by velocity.
+        this.position.x += this.velocity.x * delta.f
+        this.position.y += this.velocity.y * delta.f
+
+        // Deceleration of velocity.
+        this.velocity.x *= FRICTION
+        this.velocity.y *= FRICTION
     }
 }
