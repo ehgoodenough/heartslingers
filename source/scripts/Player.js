61e06c046bb11d5f26d9672ea86caeb33061b4d8
David McPherson <DavidMcP555@gmail.com>
2017-07-15 14:03:19 +0200

    resolved issue #1 : acceleration based movement

--- source/scripts/Player.js
+++ source/scripts/Player.js
@@ -5,6 +5,8 @@ import {FRAME} from "scripts/Constants.js"
 import Bullet from "scripts/Bullet.js"
 
 const FRICTION = 0.75
+const FRICTION_FALLTIME = 10 // in ticks (=1/60 of a second)
+const WALKING_RISETIME  = 10 // in ticks (=1/60 of a second)
 const GUN_COOLDOWN = 150 // in milliseconds
 
 Pixi.settings.SCALE_MODE = Pixi.SCALE_MODES.NEAREST
@@ -26,6 +28,12 @@ export default class Player extends Pixi.Sprite {
         this.anchor.y = 0.5
 
         this.speed = 3
+
+        // Calculate the desired force
+        // magnitudes to match target
+        // 0-to-Max speed times
+        this.friction  = this.speed / FRICTION_FALLTIME
+        this.walkForce = this.speed / WALKING_RISETIME + this.friction
 
         // The velocity will be defined
         // as a point, which is a tuple
@@ -71,16 +79,43 @@ export default class Player extends Pixi.Sprite {
 
         // Non-dynamic velocity assignment
         // TODO: https://github.com/ehgoodenough/gmtk-2017/issues/1
-        this.velocity.x = unitX * this.speed
-        this.velocity.y = unitY * this.speed
+        //this.velocity.x = unitX * this.speed
+        //this.velocity.y = unitY * this.speed
+
+        // Dynamic velocity assignment
+        // Player walking force
+        this.velocity.x += unitX * this.walkForce * delta.f
+        this.velocity.y += unitY * this.walkForce * delta.f
+        // Friction-based deceleration
+        var speed = vecLength(this.velocity.x,this.velocity.y) // Get velocity length to normalize a directional unit vector
+        // The friction force points
+        // opposite the current
+        // velocity at a fixed
+        // magnitude of this.friction
+        if(speed>0){
+            if(speed>this.friction * delta.f){
+              this.velocity.x -= (this.velocity.x/speed) * this.friction * delta.f
+              this.velocity.y -= (this.velocity.y/speed) * this.friction * delta.f
+            }
+            else{
+              this.velocity.x = 0
+              this.velocity.y = 0
+            }
+        }
+        // Cap the speed at the max speed
+        speed = vecLength(this.velocity.x,this.velocity.y)
+        if(speed>this.speed){
+            this.velocity.x *= (this.speed/speed)
+            this.velocity.y *= (this.speed/speed)
+        }
+
+        // Deceleration of velocity.
+        //this.velocity.x *= FRICTION
+        //this.velocity.y *= FRICTION
 
         // Translation of position by velocity.
         this.position.x += this.velocity.x * delta.f
         this.position.y += this.velocity.y * delta.f
-
-        // Deceleration of velocity.
-        this.velocity.x *= FRICTION
-        this.velocity.y *= FRICTION
     }
     shoot(delta) {
         if(this.gun.cooldown > 0) {
