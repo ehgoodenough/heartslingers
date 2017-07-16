a666d954599de5c8054f90c12aa633186934a194
David McPherson <DavidMcP555@gmail.com>
2017-07-16 11:14:15 +0200

    added basic enemy firing + damaging

--- source/scripts/Baddie.js
+++ source/scripts/Baddie.js
@@ -4,11 +4,13 @@ Pixi.settings.SCALE_MODE = Pixi.SCALE_MODES.NEAREST
 import {getVectorLength} from "scripts/Geometry.js"
 import {FRAME} from "scripts/Constants.js"
 import Bullet from "scripts/Bullet.js"
+import BaddieBullet from "scripts/BaddieBullet.js"
 import Scene from "scripts/Scene.js"
 import Text from "scripts/Text.js"
 
 const BADDIE_TEXTURE = Pixi.Texture.from(require("images/player.png"))
 const DEATH_SOUND = new Audio(require("sounds/explosion.wav"))
+const GUN_COOLDOWN = 150 // in milliseconds
 
 const ROTATIONAL_FRICTION = 0.95
 
@@ -26,6 +28,15 @@ export default class Baddie extends Pixi.Sprite {
 
         this.hearts = 5
 
+        this.isDead = false
+        this.gun = {
+            // This is the duration of
+            // time until the gun can shoot
+            // another shot. This time is
+            // in milliseconds.
+            cooldown: 0,
+        }
+
         this.velocity = new Pixi.Point()
     }
     update(delta) {
@@ -38,6 +49,33 @@ export default class Baddie extends Pixi.Sprite {
         this.velocity.r *= ROTATIONAL_FRICTION
         if(this.velocity.r <= 0.0005) {
             this.velocity.r = 0
+        }
+
+        if(this.isDead == false) {
+            this.shoot(delta)
+        }
+    }
+    // MOVE FUNCTION GOES HERE
+    shoot(delta) {
+        if(this.gun.cooldown > 0) {
+            this.gun.cooldown -= delta.ms
+        }
+
+        // If the user is holding the shoot key...
+        if(true) {
+            // And the gun has cooled down...
+            if(this.gun.cooldown <= 0) {
+                // Then heat up the gun again!
+                this.gun.cooldown = GUN_COOLDOWN
+
+                // And fire a shot.
+                if(this.parent != undefined) {
+                    this.parent.addChild(new BaddieBullet({
+                        position: this.position,
+                        direction: 3.1415
+                    }), 0)
+                }
+            }
         }
     }
     loseHeart(amount) {
